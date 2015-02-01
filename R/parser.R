851adb88e096c50b6bd6d4dd0b82100525c28347
Ambuj-UF <sendambuj@gmail.com>
2015-01-31 20:36:30 -0500

    seqfetch

--- R/parser.R
+++ R/parser.R
@@ -9,7 +9,7 @@ trim_flag <- function (x, flagger) gsub(trail(flagger), "", gsub(flagger, "", x)
 blast_parser <- function(filename) {
     text <- readLines(filename,encoding="UTF-8")
     object_vector <- list()
-
+    
     hitFlag = 0
     for (lines in text) {
         if (isTRUE(grepl("<Hit_id>", lines))) {
@@ -17,7 +17,7 @@ blast_parser <- function(filename) {
             id = trim(trim_flag(lines, "<Hit_id>"))
             object_vector[[id]] <- list()
         }
-    
+        
         if (hitFlag == 1) {
             if (isTRUE(grepl("<Hsp_evalue>", lines))) {
                 object_vector[[id]]["eval"] = trim(trim_flag(lines, "<Hsp_evalue>"))
@@ -30,10 +30,13 @@ blast_parser <- function(filename) {
             }
             if (isTRUE(grepl("<Hsp_hit-frame>", lines))) {
                 object_vector[[id]]["frame"] = trim(trim_flag(lines, "<Hsp_hit-frame>"))
+            }
+            if (isTRUE(grepl("<Hsp_hseq>", lines))) {
+                object_vector[[id]]["seq"] = trim(trim_flag(lines, "<Hsp_hseq>"))
                 hitFlag = 0
             }
         }
     }
-
+    
     return(object_vector)
 }
