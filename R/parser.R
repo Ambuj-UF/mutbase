ea536a62bff2c28a8be6b20c2e4780df16c05305
Ambuj-UF <sendambuj@gmail.com>
2015-02-01 17:38:42 -0500

    dev

--- R/parser.R
+++ R/parser.R
@@ -32,15 +32,28 @@ blast_parser <- function(filename) {
     hitFlag = 0
     for (lines in text) {
         if (isTRUE(grepl("<Hit_id>", lines))) {
-            hitFlag <- 1; id = trim(trim_flag(lines, "<Hit_id>")); object_vector[[id]] <- list()
+            hitFlag <- 1
+            id = trim(trim_flag(lines, "<Hit_id>"))
+            object_vector[[id]] <- list()
         }
         
         if (hitFlag == 1) {
-            if (isTRUE(grepl("<Hsp_evalue>", lines))) { object_vector[[id]]["eval"] = trim(trim_flag(lines, "<Hsp_evalue>"))}
-            if (isTRUE(grepl("<Hsp_hit-from>", lines))) { object_vector[[id]]["start"] = trim(trim_flag(lines, "<Hsp_hit-from>"))}
-            if (isTRUE(grepl("<Hsp_hit-to>", lines))) { object_vector[[id]]["stop"] = trim(trim_flag(lines, "<Hsp_hit-to>"))}
-            if (isTRUE(grepl("<Hsp_hit-frame>", lines))) { object_vector[[id]]["frame"] = trim(trim_flag(lines, "<Hsp_hit-frame>"))}
-            if (isTRUE(grepl("<Hsp_hseq>", lines))) { object_vector[[id]]["seq"] = trim(trim_flag(lines, "<Hsp_hseq>")); hitFlag = 0}
+            if (isTRUE(grepl("<Hsp_evalue>", lines))) {
+                object_vector[[id]]["eval"] = trim(trim_flag(lines, "<Hsp_evalue>"))
+            }
+            if (isTRUE(grepl("<Hsp_hit-from>", lines))) {
+                object_vector[[id]]["start"] = trim(trim_flag(lines, "<Hsp_hit-from>"))
+            }
+            if (isTRUE(grepl("<Hsp_hit-to>", lines))) {
+                object_vector[[id]]["stop"] = trim(trim_flag(lines, "<Hsp_hit-to>"))
+            }
+            if (isTRUE(grepl("<Hsp_hit-frame>", lines))) {
+                object_vector[[id]]["frame"] = trim(trim_flag(lines, "<Hsp_hit-frame>"))
+            }
+            if (isTRUE(grepl("<Hsp_hseq>", lines))) {
+                object_vector[[id]]["seq"] = trim(trim_flag(lines, "<Hsp_hseq>"))
+                hitFlag = 0
+            }
         }
     }
     
