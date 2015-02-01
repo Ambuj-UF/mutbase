ef1b54103c16a41bf2ccd7ca7c0eb5ca3a7f5316
Ambuj-UF <sendambuj@gmail.com>
2015-01-31 20:48:39 -0500

    dev

--- R/urlhandle.R
+++ R/urlhandle.R
@@ -1,3 +1,26 @@
+################################################################################################################
+#                                                                                                              #
+# Copyright (C) {2014}  {Ambuj Kumar, Kimball-Brain lab group, Biology Department, University of Florida}      #
+#                                                                                                              #
+# This program is free software: you can redistribute it and/or modify                                         #
+# it under the terms of the GNU General Public License as published by                                         #
+# the Free Software Foundation, either version 3 of the License, or                                            #
+# (at your option) any later version.                                                                          #
+#                                                                                                              #
+# This program is distributed in the hope that it will be useful,                                              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                                               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                #
+# GNU General Public License for more details.                                                                 #
+#                                                                                                              #
+# This program comes with ABSOLUTELY NO WARRANTY;                                                              #
+# This is free software, and you are welcome to redistribute it                                                #
+# under certain conditions;                                                                                    #
+#                                                                                                              #
+################################################################################################################
+
+
+
+
 require(RCurl)
 require(XML)
 
